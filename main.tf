74737690d7c698664a30f5041ae36bb81989b37c
erickkendall <ekendall0013@gmail.com>
2020-04-26 15:37:34 -0500

    added default security group to allow yum update

--- main.tf
+++ main.tf
@@ -5,13 +5,24 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami                    = "ami-0f7919c33c90f5b58"
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+  vpc_security_group_ids = [aws_security_group.instance.id, "sg-52eb493b"]
+  key_name      = aws_key_pair.mykey.key_name
+
 
   user_data = <<-EOF
     #!/bin/bash
-    echo "Hello World!" > index.html
-    nohup busybox httpd -f -p var.server_port &
+    sudo yum update
+    sudo yum install httpd -y
+    echo "Hello World!" > /var/www/html/index.html
+    sudo service httpd start
     EOF
+
+  connection {
+    user        = var.INSTANCE_USERNAME
+    private_key = file(var.PATH_TO_PRIVATE_KEY)
+    host        = self.public_ip
+  }
+
 
   tags = {
     Name = "terraform-example"
@@ -28,7 +39,19 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
+
+resource "aws_key_pair" "mykey" {
+  key_name   = "mykey"
+  public_key = file(var.PATH_TO_PUBLIC_KEY)
+}
+
 
 output "public_ip" {
   value = aws_instance.example.public_ip
--- vars.tf
+++ vars.tf
@@ -2,3 +2,14 @@ variable "server_port" {
   description = "The port the server will use for HTTP request"
   default     = 8083
 }
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ec2-user"
+}
