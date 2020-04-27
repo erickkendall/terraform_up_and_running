f5a891c096458b34f6741f05ac3c0bf237c3b2e9
erickkendall <ekendall0013@gmail.com>
2020-04-27 09:25:42 -0500

    utilizing cloudposse

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example" {
   ami                    = "ami-0f7919c33c90f5b58"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id, "sg-52eb493b"]
-  key_name      = aws_key_pair.mykey.key_name
+  key_name               = aws_key_pair.mykey.key_name
 
 
   user_data = <<-EOF
@@ -55,4 +55,25 @@ resource "aws_key_pair" "mykey" {
 
 output "public_ip" {
   value = aws_instance.example.public_ip
+}
+
+module "vpc" {
+  source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=master"
+  namespace  = "eg"
+  stage      = "test"
+  name       = "app"
+  cidr_block = "10.240.76.0/23"
+}
+
+module "dynamic_subnets" {
+  source                  = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=master"
+  namespace               = "eg"
+  stage                   = "test"
+  name                    = "app"
+  availability_zones      = ["us-east-2a", "us-east-2b", "us-east-2c"]
+  vpc_id                  = module.vpc.vpc_id
+  igw_id                  = module.vpc.igw_id
+  cidr_block              = "10.240.76.0/23"
+  map_public_ip_on_launch = "false"
+  max_subnet_count        = 3
 }
